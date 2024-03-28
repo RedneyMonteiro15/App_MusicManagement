43a12028d9efefe9221e5e08d375a85b64b85586
Redney Monteiro <redneymonteiro15@gmail.com>
2024-03-28 17:11:02 +0000

    template login, register, splashscreen

--- App/app/src/main/java/com/example/musicmanagement/SplashScreen.kt
+++ App/app/src/main/java/com/example/musicmanagement/SplashScreen.kt
@@ -1,11 +1,19 @@
 package com.example.musicmanagement
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val i = Intent(this, Login::class.java)
+            startActivity(i)
+        }, 3000)
     }
 }
